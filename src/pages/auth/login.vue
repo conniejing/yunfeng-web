5f64e5114cc89de3fd04a89e1a3ff447f3955b9b
zhoujing <jing.zhou@beecloud.com>
2017-05-09 15:32:35 +0800

    init

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,19 +1,36 @@
 <template>
-  <div>
-    {{msg}}
-    <button>
-      <router-link to="/">点击我跳转</router-link>
-    </button>
+  <div @keyup.enter="login()" style="margin-top:150px;">
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="9">
+          <el-form :model="loginInfo" ref="loginInfo" label-width="100px">
+          <el-form-item label="用户名">
+            <el-input type="text" v-model.trim="loginInfo.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input type="password"  v-model.trim="loginInfo.password" auto-complete="new-password"></el-input>
+          </el-form-item>
+          <el-form-item class="text-center">
+            <el-button type="primary" @click="login()">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
   export default {
     data () {
       return {
-        msg: '我是登录页面'
+        loginInfo: {
+          username: '',
+          password: ''
+        }
       }
     },
     methods: {
+      login () {
+        this.$router.push('/')
+      }
     }
   }
 </script>
